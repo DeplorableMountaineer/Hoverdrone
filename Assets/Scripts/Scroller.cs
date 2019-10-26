6dc9cbe6cce934502454016c84bfc1e65c2416cb
tdvance <deplorable.mountaineer@gmail.com>
2019-10-26 12:59:08 -0400

    Scrolling background

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+    [SerializeField] private Vector2 scrollVelocity = Vector2.right*0.03125f;
+
+    private Material _material;
+
+    // Start is called before the first frame update
+    void Start() {
+        _material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        _material.mainTextureOffset = scrollVelocity*Time.time;
+    }
+}
