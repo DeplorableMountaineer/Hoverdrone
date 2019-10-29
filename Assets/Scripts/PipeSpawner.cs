c9bd0441f9ec505e898e06bba6cb6b7aab565d61
tdvance <deplorable.mountaineer@gmail.com>
2019-10-29 17:58:37 -0400

    Pipes spawn in pairs

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -6,8 +6,12 @@ public class PipeSpawner : MonoBehaviour {
     [SerializeField] private float minSeconds = 2;
     [SerializeField] private float maxSeconds = 3;
 
-    [SerializeField] private float minY = -383;
-    [SerializeField] private float maxY = -27;
+    [SerializeField] private float minMidpoint = -384;
+    [SerializeField] private float maxMidpoint = 384;
+
+    [SerializeField] private float minHalfGap = 50;
+    [SerializeField] private float maxHalfGap = 150;
+    [SerializeField] private float pipeHalfHeight = 192;
 
     [SerializeField] private GameObject pipePrefab;
 
@@ -15,6 +19,7 @@ public class PipeSpawner : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        Random.InitState((int)System.DateTime.Now.Ticks);
         if(spawning) {
             StartSpawning();
         }
@@ -32,12 +37,15 @@ public class PipeSpawner : MonoBehaviour {
     private IEnumerator Spawn() {
         do {
             yield return new WaitForSeconds(Random.Range(minSeconds, maxSeconds));
-            float y = Random.Range(minY, maxY);
-            Vector3 pos = new Vector3(transform.position.x, y, transform.position.z);
+            float halfGap = Random.Range(minHalfGap, maxHalfGap);
+            float midpoint = Random.Range(minMidpoint + halfGap, maxMidpoint - halfGap);
+            Vector3 pos = new Vector3(transform.position.x, midpoint - halfGap - pipeHalfHeight, transform.position.z);
+            Vector3 pos2 = new Vector3(transform.position.x, midpoint + halfGap + pipeHalfHeight, transform.position.z);
+
             if(spawning) {
                 Instantiate(pipePrefab, pos, Quaternion.identity);
+                Instantiate(pipePrefab, pos2, Quaternion.Euler(0, 0, 180));
             }
         } while(spawning);
     }
 }
- 
