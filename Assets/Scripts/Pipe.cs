ded9f9596ef60f28f96a72564f33e07b5a12a20c
tdvance <deplorable.mountaineer@gmail.com>
2019-10-29 12:16:15 -0400

    Added scrolling pipes from ground

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+    [SerializeField] private Vector2 moveVelocity = Vector2.left*0.0625f*48f;
+    [SerializeField] private float maxY = -27;
+    [SerializeField] private float minY = -383;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = moveVelocity;
+    }
+}
---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour {
+    [SerializeField] private float minSeconds = 2;
+    [SerializeField] private float maxSeconds = 3;
+
+    [SerializeField] private float minY = -383;
+    [SerializeField] private float maxY = -27;
+
+    [SerializeField] private GameObject pipePrefab;
+
+    [SerializeField] private bool spawning = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        if(spawning) {
+            StartSpawning();
+        }
+    }
+
+    public void StartSpawning() {
+        spawning = true;
+        StartCoroutine(Spawn());
+    }
+
+    public void StopSpawning() {
+        spawning = false;
+    }
+
+    private IEnumerator Spawn() {
+        do {
+            yield return new WaitForSeconds(Random.Range(minSeconds, maxSeconds));
+            float y = Random.Range(minY, maxY);
+            Vector3 pos = new Vector3(transform.position.x, y, transform.position.z);
+            if(spawning) {
+                Instantiate(pipePrefab, pos, Quaternion.identity);
+            }
+        } while(spawning);
+    }
+}
+ 
